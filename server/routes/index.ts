9f603c3e0f26c73f78a83bab1059ec7fe8011569
zaher-aa <zaherabuamro@gmail.com>
2022-04-21 20:41:08 +0300

    Added authentication router Relates #5

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import authRouter from './auth';
 import studentRouter from './student';
 import teacherRouter from './teacher';
 
 const router = Router();
 
+router.use('/auth', authRouter);
 router.use('/student', studentRouter);
 router.use('/teacher', teacherRouter);
 
