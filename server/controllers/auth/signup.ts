ea7d22dbd0bc4ea3bc19775dfa52f8081ba333a8
zaher-aa <zaherabuamro@gmail.com>
2022-04-22 18:11:13 +0300

    Hash the user password to store it in the database Relates #15

--- server/controllers/auth/signup.ts
+++ server/controllers/auth/signup.ts
@@ -1,4 +1,5 @@
 import { Response, Request, NextFunction } from 'express';
+import { hash } from 'bcrypt';
 import { createNewUserQuery, checkEmailTakenQuery } from '../../database/queries';
 import { signupSchema, signToken } from '../../utils';
 import { CustomError } from '../../errors';
@@ -6,16 +7,25 @@ import { CustomError } from '../../errors';
 export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const {
-      role, email, userId, username,
+      role, email, username, password, bio, avatar,
     } = req.body;
 
     await signupSchema.validate({ ...req.body, role });
     const destination = role === 'student' ? 'students' : 'teachers';
     const { rowCount: isEmailTaken } = await checkEmailTakenQuery({ destination, email });
     if (isEmailTaken) throw new CustomError('The email you used is taken', 409);
-    const { rows } = await createNewUserQuery({ destination, ...req.body });
+    const hashedPassword = await hash(password, 10);
+    const { rows } = await createNewUserQuery({
+      destination,
+      email,
+      username,
+      password: hashedPassword,
+      bio,
+      avatar,
+    });
     const user = rows[0];
-    const token = await signToken({ userId, username, role });
+    const token = await signToken({ userId: user.id, username, role });
+
     res
       .status(201)
       .cookie('token', token)
