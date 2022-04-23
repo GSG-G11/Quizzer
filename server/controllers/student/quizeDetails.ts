7aa67cb5816637e904e7969145eb62df0ec5fd32
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-04-23 15:26:30 +0300

    add quiz data to query

--- server/controllers/student/quizeDetails.ts
+++ server/controllers/student/quizeDetails.ts
@@ -5,10 +5,8 @@ import { CustomError } from '../../errors';
 export default async ({ params, cookies }:Request, res:Response, next:NextFunction) => {
   try {
     const { rows } = await quizDetails(params.quizId);
-    console.log(rows);
     const { token } = cookies;
     if (!token) throw new CustomError('Unauthorized', 401);
-    console.log('rowsss', token);
 
     res.json({ data: rows, message: 'Success' });
   } catch (err) {
--- server/database/queries/student/quiz-details-query.ts
+++ server/database/queries/student/quiz-details-query.ts
@@ -2,6 +2,6 @@ import connections from '../../config/connections';
 
 export default (id:String) => connections
   .query(
-    'SELECT * FROM questions WHERE quiz_id = $1',
+    'SELECT questions.*, quizzes.title AS quiz_title, quizzes.quiz_mark, quizzes.time AS quiz_time FROM questions JOIN quizzes ON questions.quiz_id = quizzes.id WHERE quiz_id = $1',
     [id],
   );
