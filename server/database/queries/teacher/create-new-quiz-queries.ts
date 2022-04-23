42d3beeec08677786d781898a91df266936b6ed3
zaher-aa <zaherabuamro@gmail.com>
2022-04-23 18:16:12 +0300

    Added create-new-quiz controller quiries Relates #42

---
+++ server/database/queries/teacher/create-new-quiz-queries.ts
@@ -0,0 +1,25 @@
+import dbConnection from '../../config/connections';
+
+const createQuizQuery = ({
+  quizId, teacherId, title, description, mark, time,
+}) => dbConnection.query(
+  `
+    INSERT INTO quizzes (id, teacher_id, title, description, mark, time) VALUES
+      ($1, $2, $3, $4, $5, $6)
+      RETURNING *;
+  `,
+  [quizId, teacherId, title, description, mark, time],
+);
+
+const createQuestionQuery = ({
+  quizId, question, type, answers,
+}) => dbConnection.query(
+  `
+    INSERT INTO questions (quiz_id, question, type, answers) VALUES
+      ($1, $2, $3, $4)
+      RETURNING *;
+  `,
+  [quizId, question, type, answers],
+);
+
+export { createQuizQuery, createQuestionQuery };
