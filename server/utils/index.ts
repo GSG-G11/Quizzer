ece30ccd86777877db64b976e2dd1c63eecaf418
zaher-aa <zaherabuamro@gmail.com>
2022-04-23 12:55:43 +0300

    Edited verifyToken function parameters Relates #38

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,5 +1,6 @@
 export { signupSchema } from './validation';
 export { signToken, verifyToken } from './jwt';
+export { default as emailResponse } from './email-response';
 export {
   successSignup,
   takenEmail,
