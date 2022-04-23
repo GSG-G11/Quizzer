ef97fe69056e03f84c03af794e0df48afe5d3cdd
zaher-aa <zaherabuamro@gmail.com>
2022-04-23 12:55:57 +0300

    Relates #38

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,1 +1,2 @@
 export { signup, logOut } from './auth';
+export { addPrivateQuizScore, sendEmail, checkUserAttendQuiz } from './student';
--- server/routes/student/index.ts
+++ server/routes/student/index.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { checkUserAttendQuiz, addPrivateQuizScore, sendEmail } from '../../controllers';
+import { checkAuth } from '../../middlewares';
 
 const router = Router();
+
+router.post('/score', checkAuth('student'), checkUserAttendQuiz, addPrivateQuizScore, sendEmail);
 
 export default router;
