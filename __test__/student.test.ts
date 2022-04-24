843434d898ba9a5859365efd76fb0ce6dc11061a
amjad <amjedyehia1998@gmail.com>
2022-04-24 13:32:42 +0300

    changed response from object propery key to quiz instead of data relates #29

--- __test__/student.test.ts
+++ __test__/student.test.ts
@@ -10,12 +10,12 @@ const baseURL = '/api/v1/student';
 
 describe('/api/v1/student', () => {
   it('should return 200 and quiz data as json response', async () => {
-    const { body: { data } } = await supertest(app)
+    const { body: { quiz } } = await supertest(app)
       .get(`${baseURL}/quiz/quiz-1`)
       .expect(200)
       .expect('Content-Type', /json/);
 
-    const actual = data;
+    const actual = quiz;
     const expected = {
       description: 'This is quiz 1',
       id: 'quiz-1',
