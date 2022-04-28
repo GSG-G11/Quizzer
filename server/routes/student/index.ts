ad84f997e16a0b57f1fde43ab70a1d52e0e738e7
amjad <amjedyehia1998@gmail.com>
2022-04-28 13:49:25 +0300

    remove extra route

--- server/routes/student/index.ts
+++ server/routes/student/index.ts
@@ -9,10 +9,12 @@ import { checkAuth } from '../../middlewares';
 
 const router = Router();
 
-router.route('/leaderboard/:quizTitle').get(leaderboard).post(checkAuth('student'), updateLeaderboard);
+router
+  .route('/leaderboard/:quizTitle')
+  .get(leaderboard)
+  .post(checkAuth('student'), updateLeaderboard);
 
 router.get('/questions/:quizId', checkAuth('student'), getQuestions);
-router.route('/leaderboard/:quizTitle').get(leaderboard);
 
 router.post('/score', checkAuth('student'), checkUserAttendQuiz, addPrivateQuizScore, sendEmail);
 router.get('/quiz/:quizId', getQuiz);
--- server/utils/validation/update-leaderboard-schema.ts
+++ server/utils/validation/update-leaderboard-schema.ts
@@ -14,5 +14,5 @@ const quizzesTitles = [
 
 export default object({
   quizTitle: string().oneOf(quizzesTitles).required('a valid Quiz title is required'),
-  score: number().min(0, 'score must be greater than 0').max(10, 'score must be less than 10').required('score is required'),
+  score: number().min(0, 'score must be 0 or greater').max(10, 'score must be 10 or less').required('score is required'),
 });
