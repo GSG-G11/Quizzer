d59e79a92a5d5903ef9081837d45234a970258db
zaher-aa <zaherabuamro@gmail.com>
2022-05-07 11:25:12 +0300

    get id instead of the whole row (#69)

--- server/queries/teacher/check-quiz-exists.ts
+++ server/queries/teacher/check-quiz-exists.ts
@@ -2,7 +2,7 @@ import dbConnection from '../../database/connections';
 
 export default (quizId: string) => dbConnection.query(
   `
-    SELECT * FROM quizzes WHERE id = $1
+    SELECT id FROM quizzes WHERE id = $1
   `,
   [quizId],
 );
