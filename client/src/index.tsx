67829ed071ba4d5f40cbcf0ab742cdb0b7884260
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-05-11 14:27:18 +0300

    feat(auth): disable react devtools when in production (#85)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from './mui';
 import theme from './theme';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 const app = (
