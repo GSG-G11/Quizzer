0da18be8cdbadcbdd49896ba1b97f42020297086
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-05-14 08:44:20 +0300

    fix: fix navbar integration with the auth provider

--- client/src/Components/Navbar/MenuList.tsx
+++ client/src/Components/Navbar/MenuList.tsx
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import {
   Divider, Menu, MenuItem, Typography, Stack,
 } from '@mui/material';
-import { properCase } from '../../utils';
+import { properCase } from '../../Utils';
 import { useAuth } from '../../Hooks';
 
 import { IMenuList } from './Interfaces';
 
 function MenuList({ setDrawer, toggleMenu, anchorEl }:IMenuList) {
-  const { user: { role, username }, logout } = useAuth();
+  const { user, logout } = useAuth();
+  const { username, role } = user || {};
+
   const navigate = useNavigate();
 
   const hideDrawerAndMenu = () => {
@@ -22,10 +24,10 @@ function MenuList({ setDrawer, toggleMenu, anchorEl }:IMenuList) {
       <Stack divider={<Divider orientation="horizontal" flexItem />} width="10rem">
         <Typography variant="overline" color="GrayText" textAlign="center" component="span">
           Signed in as
-          <Typography color="initial" variant="subtitle2" mb="0.5rem">{properCase(username)}</Typography>
+          <Typography color="initial" variant="subtitle2" mb="0.5rem">{properCase(username || '')}</Typography>
         </Typography>
 
-        {role === 'teacher' && <MenuItem onClick={() => { hideDrawerAndMenu(); navigate('profile'); }}>Profile</MenuItem>}
+        {role === 'teacher' && <MenuItem onClick={() => { hideDrawerAndMenu(); navigate('/teacher/profile'); }}>Profile</MenuItem>}
         <MenuItem onClick={() => { hideDrawerAndMenu(); logout(); }}>Logout</MenuItem>
       </Stack>
     </Menu>
