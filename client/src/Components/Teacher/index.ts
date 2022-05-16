1779fb613324c1084c840dffa81e65cb1a65400c
zaher-aa <zaherabuamro@gmail.com>
2022-05-16 15:43:44 +0300

    fix: removed quiz type modal files (#81)

--- client/src/Components/Teacher/index.ts
+++ client/src/Components/Teacher/index.ts
@@ -1,4 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as CreateQuizForm } from './CreateQuizForm';
-export { default as QuestionTypeModal } from './QuestionTypeModal';
 export { default as QuestionFormWrapper } from './QuestionFormWrapper';
--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -2,4 +2,4 @@ export { default as RoleModal } from './RoleModal';
 export { PrivateQuizForm } from './Student';
 export { default as Navbar } from './Navbar';
 export { default as SnackBarProvider } from './SnackBar';
-export { QuestionFormWrapper, QuestionTypeModal, CreateQuizForm } from './Teacher';
+export { QuestionFormWrapper, CreateQuizForm } from './Teacher';
