d040babdc730f79dc94ddd38083773084c18a675
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-05-19 11:50:10 +0300

    feat: edit profile backend (#123)

---
+++ server/utils/models/teacher-profile-edited.ts
@@ -0,0 +1,19 @@
+const teacherInfoSuccessEdited = {
+  username: 'Username is updated',
+  bio: 'bio is updated',
+  avatar: 'http://avatar.png',
+};
+
+const teacherNameError = {
+  username: '',
+  bio: 'bio is updated',
+  avatar: 'http://avatar.png',
+};
+
+const teacherImageError = {
+  username: 'Username is updated',
+  bio: 'bio is updated',
+  avatar: 'avatar is updated',
+};
+
+export { teacherInfoSuccessEdited, teacherNameError, teacherImageError };
--- server/validation/index.ts
+++ server/validation/index.ts
@@ -4,3 +4,4 @@ export { default as addQuizSchema } from './add-quiz-schema';
 export { default as quizIdSchema } from './quiz-id-schema';
 export { default as addScoreSchema } from './add-score-schema';
 export { default as updateLeaderboardSchema } from './update-leaderboard-schema';
+export { default as editProfileSchema } from './edit-teacher-profile-schema';
